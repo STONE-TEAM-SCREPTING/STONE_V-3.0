16c01d70bb5396c722ab5e8b65f71085b759ad42
STONE-TEAM-SCREPTING <96343626+STONE-TEAM-SCREPTING@users.noreply.github.com>
2022-01-04 18:00:43 +0200

    Add files via upload

---
+++ o.py
@@ -0,0 +1,225 @@
+import os
+import time
+os.system ('clear')
+def banner ():
+    import os
+    import time
+    time.sleep (0.2)
+    print (r"""                            ,-.
+       ___,---.__          /'|`\          __,---,___
+    ,-'    \`    `-.____,-'  |  `-.____,-'    //    `-.
+  ,'        |           ~'\     /`~           |        `.
+ /      ___//              `. ,'          ,  , \___      \
+|    ,-'   `-.__   _         |        ,    __,-'   `-.    |
+|   /          /\_  `   .    |    ,      _/\          \   |
+\  |           \ \`-.___ \   |   / ___,-'/ /           |  /
+ \  \           | `._   `\\  |  //'   _,' |           /  /
+  `-.\         /'  _ `---'' , . ``---' _  `\         /,-'
+     ``       /     \    ,='/ \`=.    /     \       ''
+             |__   /|\_,--.,-.--,--._/|\   __|
+             /  `./  \\`\ |  |  | /,//' \,'  \
+            /   /     ||--+--|--+-/-|     \   \
+           |   |     /'\_\_\ | /_/_/`\     |   |
+            \   \__, \_     `~'     _/ .__/   /
+             `-._,-'   `-._______,-'   `-._,-'""")
+    print ("")
+    print ("")
+    print ("")
+    time.sleep (0.2)
+    print ("\033[0;91m|=====================|             |=====================|")
+    print ("\033[0;91m|\033[1;93m[1] \033[0;92mmetasploit FW    \033[0;91m|-------------|\033[1;93m[2] \033[0;92mA-RAT            \033[0;91m|")
+    print ("\033[0;91m|_____________________|             |_____________________|")
+    print ("")
+    time.sleep (0.2)
+    print ("\033[0;91m|=====================|             |=====================|")
+    print ("\033[0;91m|\033[1;93m[3] \033[0;92mKALI LINUX       \033[0;91m|-------------|\033[1;93m[4] \033[0;92mLEADER HACKING   \033[0;91m|")
+    print ("\033[0;91m|_____________________|             |_____________________|")
+    print ("")
+    time.sleep (0.2)
+    print ("\033[0;91m|=====================|             |=====================|")
+    print ("\033[0;91m|\033[1;93m[5] \033[0;92mPAYLOAD PHONE    \033[0;91m|-------------|\033[1;93m[6] \033[0;92mV7x TOOL         \033[0;91m|")
+    print ("\033[0;91m|_____________________|             |_____________________|")
+    print ("")
+    time.sleep (0.2)
+    print ("\033[0;91m|=====================|             |=====================|")
+    print ("\033[0;91m|\033[1;93m[7] \033[0;92mGROPE WHATSAPP   \033[0;91m|-------------|\033[1;93m[8] \033[0;92mV7x phishing     \033[0;91m|")
+    print ("\033[0;91m|_____________________|             |_____________________|")
+    print ("")
+    time.sleep (0.2)
+    print ("\033[0;91m|=====================|             |=====================|")
+    print ("\033[0;91m|\033[1;93m[9] \033[0;92minatall nmap     \033[0;91m|-------------|\033[1;93m[10] \033[0;92mphone sploit    \033[0;91m|")
+    print ("\033[0;91m|_____________________|             |_____________________|")
+    print ("")
+    time.sleep (0.2)
+    print ("\033[0;91m|=====================|             |=====================|")
+    print ("\033[0;91m|\033[1;93m[11] \033[0;92mip track        \033[0;91m|-------------|\033[1;93m[12] \033[0;92minstall tool-x  \033[0;91m|")
+    print ("\033[0;91m|_____________________|             |_____________________|")
+    print ("")
+    time.sleep (0.2)
+    print ("\033[0;91m|=====================|             |=====================|")
+    print ("\033[0;91m|\033[1;93m[13] \033[0;92minstall wifite  \033[0;91m|-------------|\033[1;93m[14] \033[0;92mlearn python    \033[0;91m|")
+    print ("\033[0;91m|_____________________|     |       |_____________________|")
+    time.sleep (0.2)
+    print ("\033[0;91m                            |")
+    print ("\033[0;91m                  |=====================|")
+    print ("\033[0;91m                  |     \033[1;93m[00] \033[0;92mEXIT       \033[0;91m|")
+    print ("\033[0;91m                  |_____________________|")
+    print ("")
+    print ("")
+    stn1 = input ("\033[1;93m[*] \033[1;94mCHOSE ANY TOOL--/>\033[0;92m")
+    if stn1 == '1' :
+        os.system ('clear')
+        print ("\033[0;92m[*] thank you to use tool ^_^")
+        time.sleep (1)
+        os.system ('wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh')
+        stn2 = input ("[*] DO YOU WANT TO CONTINUE (y/n)?")
+        if stn2 == "y" :
+            time.sleep (1)
+            os.system ('clear')
+            banner ()
+    if stn1 == '2' :
+        os.system ('clear')
+        print ("\033[0;92m[*] thank you to use tool ^_^")
+        time.sleep (1)
+        os.system ('git clone https://github.com/RexTheGod/A-RAT')
+        stn2 = input ("[*] DO YOU WANT TO CONTINUE (y/n)?")
+        if stn2 == "y" :
+            time.sleep (1)
+            os.system ('clear')
+            banner ()
+    if stn1 == '3' :
+        os.system ('clear')
+        print ("\033[0;92m[*] thank you to use tool ^_^")
+        time.sleep (1)
+        os.system ('wget -O install-nethunter-termux https://offs.ec/2MceZWr')
+        stn2 = input ("[*] DO YOU WANT TO CONTINUE (y/n)?")
+        if stn2 == "y" :
+            time.sleep (1)
+            os.system ('clear')
+            banner ()
+    if stn1 == '4' :
+        os.system ('clear')
+        print ("\033[0;92m[*] thank you to use tool ^_^")
+        time.sleep (1)
+        os.system ('git clone https://github.com/Ledear-Hacker/LEDEAR_HACKING')
+        stn2 = input ("[*] DO YOU WANT TO CONTINUE (y/n)?")
+        if stn2 == "y" :
+            time.sleep (1)
+            os.system ('clear')
+            banner ()
+    if stn1 == '5' :
+        os.system ('clear')
+        print ("\033[0;92m[*] thank you to use tool ^_^")
+        time.sleep (1)
+        os.system ('git clone https://github.com/Ledear-Hacker/LEDEAR_HACKING')
+        stn2 = input ("[*] DO YOU WANT TO CONTINUE (y/n)?")
+        if stn2 == "y" :
+            time.sleep (1)
+            os.system ('clear')
+            banner ()
+    if stn1 == '6' :
+        os.system ('clear')
+        print ("\033[0;92m[*] thank you to use tool ^_^")
+        time.sleep (1)
+        os.system ('git clone https://github.com/Vairous7x/V7x-Tool')
+        stn2 = input ("[*] DO YOU WANT TO CONTINUE (y/n)?")
+        if stn2 == "y" :
+            time.sleep (1)
+            os.system ('clear')
+            banner ()
+    if stn1 == '7' :
+        os.system ('clear')
+        print ("\033[0;92m[*] thank you to use tool ^_^")
+        time.sleep (1)
+        os.system ('xdg-open https://chat.whatsapp.com/LUJGs6S0CafESrTSuhfTBa')
+        stn2 = input ("[*] DO YOU WANT TO CONTINUE (y/n)?")
+        if stn2 == "y" :
+            time.sleep (1)
+            os.system ('clear')
+            banner ()
+    if stn1 == '8' :
+        os.system ('clear')
+        print ("\033[0;92m[*] thank you to use tool ^_^")
+        time.sleep (1)
+        os.system ('git clone https://github.com/Vairous7x/V7x-Fishin')
+        stn2 = input ("[*] DO YOU WANT TO CONTINUE (y/n)?")
+        if stn2 == "y" :
+            time.sleep (1)
+            os.system ('clear')
+            banner ()
+    if stn1 == '9' :
+        os.system ('clear')
+        print ("\033[0;92m[*] thank you to use tool ^_^")
+        time.sleep (1)
+        os.system ('git clone https://github.com/nmap/nmap')
+        stn2 = input ("[*] DO YOU WANT TO CONTINUE (y/n)?")
+        if stn2 == "y" :
+            time.sleep (1)
+            os.system ('clear')
+            banner ()
+    if stn1 == '10' :
+        os.system ('clear')
+        print ("\033[0;92m[*] thank you to use tool ^_^")
+        time.sleep (1)
+        os.system ('git clone https://github.com/aerosol-can/PhoneSploit')
+        stn2 = input ("[*] DO YOU WANT TO CONTINUE (y/n)?")
+        if stn2 == "y" :
+            time.sleep (1)
+            os.system ('clear')
+            banner ()
+    if stn1 == '11' :
+        os.system ('clear')
+        print ("\033[0;92m[*] thank you to use tool ^_^")
+        time.sleep (1)
+        os.system ('git clone https://github.com/rajkumardusad/IP-Tracer.git')
+        stn2 = input ("[*] DO YOU WANT TO CONTINUE (y/n)?")
+        if stn2 == "y" :
+            time.sleep (1)
+            os.system ('clear')
+            banner ()
+    if stn1 == '12' :
+        os.system ('clear')
+        print ("\033[0;92m[*] thank you to use tool ^_^")
+        time.sleep (1)
+        os.system ('git clone https://github.com/rajkumardusad/Tool-X.git')
+        stn2 = input ("[*] DO YOU WANT TO CONTINUE (y/n)?")
+        if stn2 == "y" :
+            time.sleep (1)
+            os.system ('clear')
+            banner ()
+    if stn1 == '13' :
+        os.system ('clear')
+        print ("\033[0;92m[*] thank you to use tool ^_^")
+        time.sleep (1)
+        os.system ('git clone https://github.com/derv82/wifite2')
+        stn2 = input ("[*] DO YOU WANT TO CONTINUE (y/n)?")
+        if stn2 == "y" :
+            time.sleep (1)
+            os.system ('clear')
+            banner ()
+    if stn1 == '14' :
+        os.system ('clear')
+        print ("\033[0;92m[*] thank you to use tool ^_^")
+        time.sleep (1)
+        os.system ('xdg-open https://youtube.com/playlist?list=PLD0gjrY4-xfEbmukaOZSxJqhkt66Ey_3q')
+        stn2 = input ("[*] DO YOU WANT TO CONTINUE (y/n)?")
+        if stn2 == "y" :
+            time.sleep (1)
+            os.system ('clear')
+            banner ()
+        if stn2 == "n" :
+            os.system ('clear')
+            print ("""
+|=================================|
+|[*] THANK YOU TO USE THIS TOOL   |
+|=================================|""")
+    if stn1 == '00' :
+        os.system ('clear')
+        print ("ok......!")
+    else :
+    	print ("\033[0;92m chose any tool..!")
+    	time.sleep (1)
+    	os.system ('clear')
+    	banner ()
+banner ()
+                         
